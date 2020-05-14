55ec63cdeff7e0369c2778e0f4208592798c4795
ShunshunL <shunshunlyu@protonmail.com>
2020-05-14 11:23:12 -0700

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,54 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [],
+    eaten: [],
+    money: 100,
+    displayIndex: 0
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(res => res.json())
+    .then(res => {
+      this.setState({
+        sushis: res
+      })
+    })
+  }
+
+  eat = (sushi) => {
+    const moneyAfter = this.state.money - sushi.price
+
+    if(!this.state.eaten.includes(sushi) && moneyAfter >= 0) {
+      this.setState({
+        eaten: [...this.state.eaten, sushi],
+        money: moneyAfter
+      })
+    }
+  }
+
+  displayFourSushis = () => {
+    return this.state.sushis.slice(this.state.displayIndex, this.state.displayIndex+4)
+  }
+
+  more = (event) => {
+    let newDisplayIndex = this.state.displayIndex + 4 
+    this.setState({displayIndex: newDisplayIndex})
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer 
+          sushis={this.displayFourSushis()}
+          more={this.more}
+          eat={this.eat}
+          eaten={this.state.eaten}
+           />
+        <Table moneyLeft={this.state.money}
+               eaten={this.state.eaten}/>
       </div>
     );
   }
